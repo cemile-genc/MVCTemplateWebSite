46287023fb30b305e3f970f87ecfc4e68e96ee5a
Cemile GENÇ <cemile.genc@ithinka.com>
2022-12-28 13:30:42 +0300

    add

---
+++ MVCTemplate/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }  
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }    
+        public ActionResult Takim()
+        {
+            return View();
+        } 
+        public ActionResult Servis()
+        {
+            return View();
+        }  
+        public ActionResult Portfoy()
+        {
+            return View();
+        } 
+        public ActionResult İletisim()
+        {
+            return View();
+        }
+    }
+}
